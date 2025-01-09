c07704b1e080eb7925d5d247e2e2721a84e45de8
thetacky <ientran0822@gmail.com>
2025-01-09 02:15:59 -0800

    server running woohooooo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express'); //creates an express server
 const app = express(); //instance of express library
 const http = require("http");//creates an http server
-const {server} = require('socket.io');
+const {Server} = require('socket.io');
+const cors = require('cors');
+app.use(cors());  
 
 const server = http.createServer(app); //creates an http server
 
+//creaete new instance of the server class
 const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
-    }
+        methods: ['GET', 'POST'],
+    },
 }); //variable used to use socket.io
+
+server.listen(3001, () => {
+    console.log("server is running on port 3001");
+});
