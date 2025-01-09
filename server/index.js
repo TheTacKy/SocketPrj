88077f339812ab056b7b058cb7ae81b39fecf821
thetacky <ientran0822@gmail.com>
2025-01-09 01:57:21 -0800

    Add .gitignore and remove node_modules from version control

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); //creates an express server
+const app = express(); //instance of express library
+const http = require("http");//creates an http server
+const {server} = require('socket.io');
+
+const server = http.createServer(app); //creates an http server
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+}); //variable used to use socket.io
